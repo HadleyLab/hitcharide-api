75ea2ea2031eb86349b1291a63a623217780e1f3
Pavel Rozhkov <prozskov@gmail.com>
2018-08-31 17:30:58 +0700

    Add complete re_path to urls

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from djoser import views as djoser_views
@@ -8,7 +8,7 @@ from rest_framework_jwt.views import verify_jwt_token
 from rest_framework_jwt.views import refresh_jwt_token
 
 from apps.places.viewsets import StateViewSet, CityViewSet
-from apps.accounts.views import MyView, ValidatePhoneView
+from apps.accounts.views import MyView, ValidatePhoneView, complete
 from apps.rides.viewsets import CarViewSet, RideViewSet, \
     RideBookingViewSet, RideRequestViewSet
 
@@ -47,7 +47,10 @@ urlpatterns = [
          name='account_password_confirm'),
 
     path('accounts/rest/', include('rest_framework.urls')),
-    path('accounts/social/', include('rest_framework_social_oauth2.urls')),
+    # path('accounts/social/', include('rest_framework_social_oauth2.urls')),
+    re_path(r'^accounts/social/complete/(?P<backend>[^/]+)/$',
+            complete, name='complete'),
+    path('accounts/social/', include('social_django.urls', namespace='social')),
 ] + router.urls
 
 if settings.DEBUG:
