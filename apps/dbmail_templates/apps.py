3104bfafd8e95bce28876c54289662cd795c0254
Pavel Rozhkov <prozskov@gmail.com>
2018-09-03 21:29:42 +0700

    Create dbmail_templates app with 3 email migrations

---
+++ apps/dbmail_templates/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DBMailTemplatesConfig(AppConfig):
+    name = 'apps.dbmail_templates'
---
+++ apps/dbmail_templates/email.py
@@ -0,0 +1,30 @@
+from dbmail import send_db_mail
+from djoser.email import ActivationEmail, ConfirmationEmail, PasswordResetEmail
+from templated_mail.mail import BaseEmailMessage
+
+
+class DBMailEmail(BaseEmailMessage):
+    """
+    In this class we overrode the send method to send mail via dbmail
+    """
+    def send(self, to, *args, **kwargs):
+        super(DBMailEmail, self).send()
+        context = self.get_context_data()
+        send_db_mail(self.template_name, to, context)
+
+    def get_context_data(self):
+        context = super(DBMailEmail, self).get_context_data()
+        context.pop('view')
+        return context
+
+
+class ActivationDBMailEmail(DBMailEmail, ActivationEmail):
+    template_name = 'account_activate'
+
+
+class ConfirmationDBMailEmail(DBMailEmail, ConfirmationEmail):
+    template_name = 'account_confirmation'
+
+
+class PasswordResetEmail(DBMailEmail, PasswordResetEmail):
+    template_name = 'account_reset_password'
