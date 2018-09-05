1ff1d3ab26c63aa8c9edf407e2e0ad70c7da012b
Pavel Rozhkov <prozskov@gmail.com>
2018-09-05 17:06:50 +0700

    Add check_twilio_enabled method to accounts utils

--- apps/accounts/utils.py
+++ apps/accounts/utils.py
@@ -1,6 +1,7 @@
 from random import randint
 
 from django.core.cache import cache
+from django.conf import settings
 
 
 def random_digit(len):
@@ -23,3 +24,9 @@ def save_user_code(user_pk, code):
 
 def check_user_code(user_pk, code):
     return cache.get(get_user_code_key(user_pk)) == code
+
+
+def check_twilio_enabled():
+    return settings.TWILIO_SID and \
+           settings.TWILIO_TOKEN and \
+           settings.TWILIO_PHONE
