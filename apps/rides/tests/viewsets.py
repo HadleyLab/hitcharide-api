44ca4c54a5afe290e30e796cb81d4fae80256fa2
Pavel Rozhkov <prozskov@gmail.com>
2018-09-06 20:15:13 +0700

    Update filters, admin, factories, filters, serializers and viewsets for new rides structure

--- apps/rides/tests/viewsets.py
+++ apps/rides/tests/viewsets.py
@@ -5,7 +5,7 @@ from config.tests import APITestCase
 from apps.accounts.factories import UserFactory
 from apps.places.factories import CityFactory
 from apps.rides.factories import CarFactory, RideFactory, \
-    RideBookingFactory, RidePointFactory
+    RideBookingFactory, RideStopFactory
 from apps.rides.models import Ride, Car
 
 
@@ -18,27 +18,21 @@ class RideViewSetTest(APITestCase):
             owner=self.user,
             brand='some',
             model='car',
-            number_of_sits=5)
+            number_of_seats=5)
 
     def get_ride_data(self):
-        now = timezone.now()
         return {
-            'stops': [
-                {
-                    'city': self.city1.pk, 'cost_per_sit': 0,
-                    'order': 0, 'date_time': now + timedelta(days=1)
-                },
-                {
-                    'city': self.city2.pk, 'cost_per_sit': 100,
-                    'order': 1, 'date_time': now + timedelta(days=2)
-                },
-            ],
-            'number_of_sits': 5
+            'city_from': self.city1.pk,
+            'city_to': self.city2.pk,
+            'price': 200,
+            'date_time': timezone.now() + timedelta(days=1),
+            'stops': [],
+            'number_of_seats': 5
         }
 
     def test_create_unauthorized_forbidden(self):
         data = self.get_ride_data()
-        data.update({'car': {'pk': self.car.pk}})
+        data.update({'car': self.car.pk})
         resp = self.client.post('/rides/ride/', data, format='json')
         self.assertUnauthorized(resp)
 
@@ -46,32 +40,13 @@ class RideViewSetTest(APITestCase):
         self.authenticate()
 
         data = self.get_ride_data()
-        data.update({'car': {'pk': self.car.pk}})
+        data.update({'car': self.car.pk})
         resp = self.client.post('/rides/ride/', data, format='json')
         self.assertSuccessResponse(resp)
         ride = Ride.objects.get(pk=resp.data['pk'])
-        self.assertEqual(ride.stops.all().count(), 2)
+        self.assertEqual(ride.stops.all().count(), 0)
         self.assertEqual(ride.car.brand, 'some')
         self.assertEqual(ride.car.owner.pk, self.user.pk)
-
-    def test_create_with_nested_car(self):
-        self.authenticate()
-
-        cars_count_before = Car.objects.all().count()
-        data = self.get_ride_data()
-        data.update({'car': {
-            'brand': 'another',
-            'model': 'car',
-            'number_of_sits': 3,
-        }})
-        resp = self.client.post('/rides/ride/', data, format='json')
-        self.assertSuccessResponse(resp)
-
-        ride = Ride.objects.get(pk=resp.data['pk'])
-        self.assertEqual(ride.stops.all().count(), 2)
-        self.assertEqual(ride.car.brand, 'another')
-        self.assertEqual(ride.car.owner.pk, self.user.pk)
-        self.assertEqual(Car.objects.all().count(), cars_count_before + 1)
 
     def test_create_with_unfilled_user(self):
         self.user.phone = None
@@ -79,35 +54,32 @@ class RideViewSetTest(APITestCase):
         self.authenticate()
 
         data = self.get_ride_data()
-        data.update({'car': {'pk': self.car.pk}})
+        data.update({'car': self.car.pk})
         resp = self.client.post('/rides/ride/', data, format='json')
         self.assertBadRequest(resp)
 
     def test_update(self):
         self.authenticate()
         data = self.get_ride_data()
-        data.update({'car': {'pk': self.car.pk}})
+        data.update({'car': self.car.pk})
         resp = self.client.post('/rides/ride/', data, format='json')
         self.assertSuccessResponse(resp)
 
         ride_pk = resp.data['pk']
 
         city3 = CityFactory.create()
-        data['stops'][0]['city'] = city3.pk
         data['stops'].append({
-            'city': self.city1.pk, 'cost_per_sit': 200,
-            'order': 2, 'date_time': timezone.now() + timedelta(days=2)
+            'city': city3.pk,
+            'order': 1,
         })
         resp = self.client.put('/rides/ride/{0}/'.format(ride_pk), data,
                                format='json')
         self.assertSuccessResponse(resp)
-        self.assertEqual(len(resp.data['stops']), 3)
+        self.assertEqual(len(resp.data['stops']), 1)
         self.assertSetEqual(
             set([(stop['city'], stop['order']) for stop in resp.data['stops']]),
             {
-                (city3.pk, 0),
-                (self.city2.pk, 1),
-                (self.city1.pk, 2)
+                (city3.pk, 1),
             }
         )
 
@@ -120,34 +92,18 @@ class RideViewSetTest(APITestCase):
 
         car = CarFactory.create(owner=self.user)
         now = timezone.now()
-        tomorrow = now - timedelta(days=1)
-        yesterday = now + timedelta(days=1)
+        tomorrow = now + timedelta(days=1)
+        yesterday = now - timedelta(days=1)
 
         ride1 = RideFactory.create(
-            number_of_sits=5,
-            car=car)
-        RidePointFactory.create(
-            ride=ride1,
-            date_time=tomorrow,
-            order=0)
-        RidePointFactory.create(
-            ride=ride1,
-            date_time=yesterday,
-            order=1)
-        ride1.save()
+            number_of_seats=5,
+            car=car,
+            date_time=yesterday)
 
         ride2 = RideFactory.create(
-            number_of_sits=5,
-            car=car)
-        RidePointFactory.create(
-            ride=ride2,
-            date_time=yesterday,
-            order=0)
-        RidePointFactory.create(
-            ride=ride2,
-            date_time=yesterday,
-            order=1)
-        ride2.save()
+            number_of_seats=5,
+            car=car,
+            date_time=tomorrow)
 
         resp = self.client.get('/rides/ride/', format='json')
         self.assertSuccessResponse(resp)
@@ -165,23 +121,23 @@ class RideViewSetTest(APITestCase):
         car = CarFactory.create(owner=self.user)
 
         my_ride_1 = RideFactory.create(
-            number_of_sits=5,
+            number_of_seats=5,
             car=car)
         my_ride_2 = RideFactory.create(
-            number_of_sits=5,
+            number_of_seats=5,
             car=car)
 
         another_user = UserFactory.create()
         another_car = CarFactory.create(owner=another_user)
         RideFactory.create(
-            number_of_sits=5,
+            number_of_seats=5,
             car=another_car)
 
         resp = self.client.get('/rides/ride/my/', format='json')
         self.assertSuccessResponse(resp)
-        self.assertListEqual(
-            [my_ride_1.pk, my_ride_2.pk],
-            [ride['pk'] for ride in resp.data['results']])
+        self.assertSetEqual(
+            {my_ride_1.pk, my_ride_2.pk},
+            set([ride['pk'] for ride in resp.data['results']]))
 
 
 class RideBookingViewSetTest(APITestCase):
@@ -194,7 +150,7 @@ class RideBookingViewSetTest(APITestCase):
             owner=self.user,
             brand='some',
             model='car',
-            number_of_sits=5)
+            number_of_seats=5)
 
         self.ride = RideFactory.create(car=self.car)
         self.booking = RideBookingFactory.create(
