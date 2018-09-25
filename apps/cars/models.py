ee11a3eeacecfa331b13c8a8937d9cadfcaacd77
Pavel Rozhkov <prozskov@gmail.com>
2018-09-25 15:26:58 +0700

    Add null and blank true to car license plate

--- apps/cars/models.py
+++ apps/cars/models.py
@@ -18,7 +18,8 @@ class Car(models.Model):
     color = models.CharField(
         max_length=50)
     license_plate = models.CharField(
-        max_length=50)
+        max_length=50,
+        blank=True, null=True)
 
     def __str__(self):
         return '{0} {1} - {2}'.format(
