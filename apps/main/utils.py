a5b69c48cd91801ac2aa872a175bb60cbdbfad9c
Pavel Rozhkov <prozskov@gmail.com>
2018-09-26 16:44:08 +0700

    Add backend url to send_mail in main utils

--- apps/main/utils.py
+++ apps/main/utils.py
@@ -4,5 +4,5 @@ from dbmail import send_db_mail
 def send_mail(slug, recipient, context=None, *args, **kwargs):
     context = context or {}
     context.update({'frontend_url': '',
-                    back})
+                    'backend_url': ''})
     send_db_mail(slug, recipient, context, *args, **kwargs)
