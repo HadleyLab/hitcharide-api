8aa683b3c631864c29c63137b6753fe1fbfb1b3f
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-10-01 17:38:49 +0700

    Some list reviews tests

--- apps/reviews/tests/viewsets.py
+++ apps/reviews/tests/viewsets.py
@@ -5,6 +5,7 @@ from apps.rides.factories import RideFactory, RideBookingFactory
 from apps.cars.factories import CarFactory
 from apps.rides.models import RideBookingStatus
 from apps.reviews.models import Review, ReviewType
+from apps.reviews.factories import ReviewFactory
 
 
 class ReviewViewSetTest(APITestCase):
@@ -76,6 +77,36 @@ class ReviewViewSetTest(APITestCase):
         }, format='json')
         self.assertBadRequest(resp)
 
+    def test_create_by_passenger_about_passenger(self):
+        self.authenticate_as('passenger1@mail.ru', 'password')
+        resp = self.client.post('/reviews/', {
+            'ride': self.ride.pk,
+            'subject': self.passenger2.pk,
+            'author_type': ReviewType.PASSENGER,
+            'rating': 5
+        }, format='json')
+        self.assertBadRequest(resp)
+
+    def test_create_by_author_about_myself(self):
+        self.authenticate()
+        resp = self.client.post('/reviews/', {
+            'ride': self.ride.pk,
+            'subject': self.user.pk,
+            'author_type': ReviewType.DRIVER,
+            'rating': 5
+        }, format='json')
+        self.assertBadRequest(resp)
+
+    def test_create_by_passenger_about_myself(self):
+        self.authenticate_as('passenger1@mail.ru', 'password')
+        resp = self.client.post('/reviews/', {
+            'ride': self.ride.pk,
+            'subject': self.passenger1.pk,
+            'author_type': ReviewType.PASSENGER,
+            'rating': 5
+        }, format='json')
+        self.assertBadRequest(resp)
+
     def test_create_by_owner_success(self):
         self.authenticate()
         self.assertEqual(self.passenger1.rating, 0.0)
@@ -90,3 +121,61 @@ class ReviewViewSetTest(APITestCase):
         review = Review.objects.get(pk=resp.data['pk'])
         self.assertEqual(review.author.pk, self.user.pk)
         self.assertEqual(self.passenger1.rating, 5.0)
+
+    def test_list_unauthorized(self):
+        resp = self.client.get('/reviews/')
+        self.assertUnauthorized(resp)
+
+    def _create_reviews(self):
+        ReviewFactory.create(
+            author=self.user,
+            ride=self.ride,
+            subject=self.passenger1,
+            author_type=ReviewType.DRIVER,
+            rating=3,
+            comment='Always crying and smells like ....')
+        ReviewFactory.create(
+            author=self.user,
+            ride=self.ride,
+            subject=self.passenger2,
+            author_type=ReviewType.DRIVER,
+            rating=5,
+            comment='Nice passenger!')
+
+        ReviewFactory.create(
+            author=self.passenger1,
+            ride=self.ride,
+            subject=self.user,
+            author_type=ReviewType.PASSENGER,
+            rating=2,
+            comment='Always screaming on me!')
+        ReviewFactory.create(
+            author=self.passenger2,
+            ride=self.ride,
+            subject=self.user,
+            author_type=ReviewType.PASSENGER,
+            rating=5,
+            comment='Nice driver!')
+
+    def test_list_ride(self):
+        self._create_reviews()
+        self.authenticate()
+        new_car = CarFactory.create(owner=UserFactory.create())
+        new_ride = RideFactory.create(car=new_car)
+
+        resp = self.client.get('/reviews/?ride={0}'.format(new_ride.pk))
+        self.assertSuccessResponse(resp)
+        self.assertEqual(len(resp.data), 0)
+
+        resp = self.client.get('/reviews/?ride={0}'.format(self.ride.pk))
+        self.assertSuccessResponse(resp)
+        self.assertEqual(len(resp.data), 4)
+
+        self.assertEqual(self.user.rating, 3.5)
+
+    def test_list_author(self):
+        self._create_reviews()
+        self.authenticate()
+
+    def test_list_subject(self):
+        pass
