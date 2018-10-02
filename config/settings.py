f401a74028f036b991cbc49454465f969338f286
Elena <vesnushkalena@gmail.com>
2018-10-02 19:07:24 +0700

    Fix social auth redirect urls

--- config/settings.py
+++ config/settings.py
@@ -219,8 +219,8 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.user.user_details',
 )
 
-LOGIN_REDIRECT_URL = 'http://localhost:3000/#/account/social-auth-success/'
-LOGIN_ERROR_URL = 'http://localhost:3000/#/account/social-auth-error/'
+LOGIN_REDIRECT_URL = 'http://localhost:3000/account/social-auth-success/'
+LOGIN_ERROR_URL = 'http://localhost:3000/account/social-auth-error/'
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.environ.get('SA_GOOGLE_OAUTH2_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.environ.get('SA_GOOGLE_OAUTH2_SECRET')
