4c669117f9f31ac3ab29d4958ac641877a6b4582
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-10-09 14:50:32 +0700

    separate minimal serializer for list

--- apps/flatpages/viewsets.py
+++ apps/flatpages/viewsets.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.permissions import AllowAny
 
 from .models import FlatPage
-from .serializers import FlatPageSerializer
+from .serializers import FlatPageSerializer, FlatPageListSerializer
 
 
 class FlatPageViewSet(viewsets.GenericViewSet,
@@ -11,3 +11,9 @@ class FlatPageViewSet(viewsets.GenericViewSet,
     queryset = FlatPage.objects.all()
     serializer_class = FlatPageSerializer
     permission_classes = (AllowAny,)
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return FlatPageListSerializer
+        else:
+            return super(FlatPageViewSet, self).get_serializer_class()
