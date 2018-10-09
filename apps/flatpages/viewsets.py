ec5402115974edfb6ca09b174d9a9c58a342516a
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-10-09 14:47:24 +0700

    Retrieve and permissions added

--- apps/flatpages/viewsets.py
+++ apps/flatpages/viewsets.py
@@ -1,10 +1,13 @@
 from rest_framework import viewsets, mixins
+from rest_framework.permissions import AllowAny
 
 from .models import FlatPage
 from .serializers import FlatPageSerializer
 
 
 class FlatPageViewSet(viewsets.GenericViewSet,
-                      mixins.ListModelMixin):
+                      mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin):
     queryset = FlatPage.objects.all()
     serializer_class = FlatPageSerializer
+    permission_classes = (AllowAny,)
