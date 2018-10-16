cae8af56805e4a6bb35301bd4cad6881a437272b
Pavel Rozhkov <prozskov@gmail.com>
2018-10-16 10:17:24 +0700

    Remove useful code from car viewsets tests

--- apps/cars/tests/viewsets.py
+++ apps/cars/tests/viewsets.py
@@ -1,25 +1,12 @@
-from datetime import timedelta
-from unittest import mock
-
-from django.utils import timezone
-
-from apps.main.test_utils import assert_mock_called_with
 from config.tests import APITestCase
-from apps.accounts.factories import UserFactory
 from apps.places.factories import CityFactory
-from apps.rides.factories import RideFactory, \
-    RideBookingFactory, RideStopFactory, RideComplaintFactory, \
-    RideRequestFactory
+from apps.rides.factories import RideFactory
 from apps.cars.factories import CarFactory
-from apps.rides.models import Ride, RideComplaintStatus, RideStatus, \
-    RideBookingStatus
 
 
 class CarViewSetTest(APITestCase):
     def setUp(self):
         super(CarViewSetTest, self).setUp()
-        self.city1 = CityFactory.create()
-        self.city2 = CityFactory.create()
         self.car = CarFactory.create(
             owner=self.user,
             brand='some',
