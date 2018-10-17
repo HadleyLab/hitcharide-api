a2fc1850d80b5b1f2ccb110fb623203a7ff5d85f
Pavel Rozhkov <prozskov@gmail.com>
2018-10-17 14:38:16 +0700

    Add filter to rides admin

--- apps/rides/admin.py
+++ apps/rides/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Ride, RideStop, RideBooking, RideComplaint, RideStatus
+from .models import Ride, RideStop, RideBooking, RideComplaint, RideStatus, \
+    RideComplaintStatus
 
 
 class RideStatusFilter(admin.SimpleListFilter):
@@ -16,8 +17,21 @@ class RideStatusFilter(admin.SimpleListFilter):
         return queryset.filter(status=self.value())
 
 
+class RideComplaintsFilter(admin.SimpleListFilter):
+    title = 'complaints'
+    parameter_name = 'complaints'
+
+    def lookups(self, request, model_admin):
+        return (
+            RideComplaintStatus.CHOICES
+        )
+
+    def queryset(self, request, queryset):
+        return queryset.filter(complaints__status=self.value())
+
+
 class RideAdmin(admin.ModelAdmin):
-    list_filter = (RideStatusFilter,)
+    list_filter = (RideStatusFilter, RideComplaintsFilter,)
 
 
 admin.site.register(RideComplaint)
