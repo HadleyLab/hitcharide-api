d9be288f0b383240001fff4a3e5dfa5c4f6dd3ea
Pavel Rozhkov <prozskov@gmail.com>
2018-10-17 13:30:57 +0700

    Add ride filter by status in admin

--- apps/rides/admin.py
+++ apps/rides/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 
-from .models import Ride, RideStop, RideBooking, RideComplaint
+from .models import Ride, RideStop, RideBooking, RideComplaint, RideStatus
+
+
+class RideStatusFilter(admin.SimpleListFilter):
+    title = 'status'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return (
+            RideStatus.CHOICES
+        )
+
+    def queryset(self, request, queryset):
+        return queryset.filter(status=self.value())
+
+
+class RideAdmin(admin.ModelAdmin):
+    list_filter = (RideStatusFilter,)
 
 
 admin.site.register(RideComplaint)
-admin.site.register(Ride)
+admin.site.register(Ride, RideAdmin)
 admin.site.register(RideStop)
 admin.site.register(RideBooking)
