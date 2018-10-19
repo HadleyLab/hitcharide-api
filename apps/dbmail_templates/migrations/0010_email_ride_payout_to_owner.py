727487e0dc4c0b155ecd8bc8808b2528ebe3b9dc
Pavel Rozhkov <prozskov@gmail.com>
2018-10-19 16:29:44 +0700

    Add your revard to ride payout to owner email

--- apps/dbmail_templates/migrations/0010_email_ride_payout_to_owner.py
+++ apps/dbmail_templates/migrations/0010_email_ride_payout_to_owner.py
@@ -16,8 +16,7 @@ def load_mail_template(apps, schema_editor):
         </p>
         <p>
         The Cost Contribution for the ride includes<br>
-        Driver reward: {{ ride.price }} $
-        Service fee: {{ ride.fee_price }} $
+        Your reward: {{ ride.total_for_driver }} $
         </p>
         <p>You can see the ride details <a href='{{ ride_detail }}'>here</a></p>
         <p>Thanks for using our site!</p>
