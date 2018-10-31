eecb11e4ec4cf9506888a6845cfcb24e83fe7bf3
ruscoder <rusmiligamer@gmail.com>
2018-10-31 19:37:57 +0700

    Add places into admin

--- apps/places/admin.py
+++ apps/places/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 
-from .models import State, City
+from .models import State, City, Place
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_filter = ('state', )
+    search_fields = ('name', )
+
+
+class PlaceAdmin(admin.ModelAdmin):
+    list_filter = ('category', )
+    search_fields = ('name', 'short_name', 'city', )
 
 
 admin.site.register(State)
-admin.site.register(City)
+admin.site.register(City, CityAdmin)
+admin.site.register(Place, PlaceAdmin)
